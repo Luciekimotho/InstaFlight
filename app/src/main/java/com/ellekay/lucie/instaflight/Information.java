491fde7bcc8a796fb5ac01bef2d99d393f6c9340
Luciekimotho <luciekimotho@gmail.com>
2015-11-01 21:00:10 +0300

    Information simple list view and adapter

--- app/src/main/java/com/ellekay/lucie/instaflight/Information.java
+++ app/src/main/java/com/ellekay/lucie/instaflight/Information.java
@@ -2,12 +2,30 @@ package com.ellekay.lucie.instaflight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Information extends AppCompatActivity {
+
+    private ListView infolistview;
+    private ArrayAdapter<String> infoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        infolistview = (ListView) findViewById(R.id.infolist);
+
+        String[] info = new String[]{"WIFI", "VENUE","PAYMENT","TRANSPORT","CONTACT"};
+        ArrayList<String> infolist = new ArrayList<String>();
+        infolist.addAll(Arrays.asList(info));
+
+        infoAdapter = new ArrayAdapter<String>(this,R.layout.information_item, infolist);
+
+        infolistview.setAdapter(infoAdapter);
     }
 }
